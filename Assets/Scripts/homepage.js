092ecdde8c387aa8a6ae8eadfdbc9645b3ee9396
Wolfyre243 <wolfyreblueflare@gmail.com>
2024-07-05 20:24:40 +0800

    solved zooming in problem, and added breakpoints

--- Assets/Scripts/homepage.js
+++ Assets/Scripts/homepage.js
@@ -6,23 +6,22 @@ window.onload = () => {
     //Variables
     let heroHeader = document.getElementById('hero-header');
     let heroFade = document.getElementById('hero-fade');
+    let heroStart = document.getElementById('hero-start');
+    let body = document.body;
 
     //Functions
 
     //Event Listeners
     document.addEventListener('scroll', () => {
-        console.log(window.scrollY);
+        //For debugging purposes
+        console.log(`Scrolling at: ${window.scrollY}, Body height: ${body.scrollHeight}`);
         /*Increase the zoom factor based on the number of pixels the element has
         scrolled so far. The height should remain the same. */
-        heroHeader.style.fontSize = (10 + window.scrollY/15) + 'vw';
-        
-        if (window.scrollY > 950) {
-            heroHeader.style.opacity -= 0.01; //change the opacity when the user has scrolled this far.
-            heroFade.style.opacity -= 0.01;
-        } else {
-            heroHeader.style.opacity = 1;
-            heroFade.style.opacity = 1;
-        }
+        let currentOpacity = 1 - ((window.scrollY/body.scrollHeight)*2);
+        heroHeader.style.fontSize = (10 + window.scrollY/10) + 'vw';
+        heroFade.style.opacity = currentOpacity;
+        heroStart.style.opacity = currentOpacity;
+        heroHeader.style.opacity = currentOpacity;
         
         console.log('poom');
     });
